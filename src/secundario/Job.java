7224712fd46babcd53b05f117a7724cfe6e45b56
alvar <alvarorgf16@gmail.com>
2023-10-07 22:53:30 +0100

    Creating job class

---
+++ src/secundario/Job.java
@@ -0,0 +1,24 @@
+package secundario;
+
+public class Job {
+    int salary;
+    int dayhours;
+    public Job() {
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getDayhours() {
+        return dayhours;
+    }
+
+    public void setDayhours(int dayhours) {
+        this.dayhours = dayhours;
+    }
+}
